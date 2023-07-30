e1785deb6f26dd7dd6bee9b12e4f6c9c91c50db8
Mohit229123 <83329568+Mohit229123@users.noreply.github.com>
2023-07-30 17:38:53 +0530

    Updated metadata.sh

--- metadata.sh
+++ metadata.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-# Function to retrieve instance metadata using curl and jq
+# Fetch the entire metadata
 get_instance_metadata() {
-    # Fetch the instance ID using curl
-    local instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-
-    # Fetch instance identity document using curl
-    local instance_metadata=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document)
-    
-    # Combine instance ID with other metadata using jq
-    local metadata="{\"InstanceId\":\"$instance_id\", $(echo "$instance_metadata" | jq -r '. | del(.instanceId)')}"
-    
-    echo "$metadata"
+  local metadata
+  metadata=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document)
+  echo "${metadata}" | jq '.'
 }
 
-# Main function
-main() {
-    # Get the instance metadata
-    instance_metadata=$(get_instance_metadata)
-    
-    if [ -n "$instance_metadata" ]; then
-        # Print the metadata in JSON format using jq
-        echo "$instance_metadata" | jq .
-    else
-        echo "Failed to retrieve instance metadata."
-        exit 1
-    fi
+# Fetch a specific data key from the metadata
+get_specific_metadata_key() {
+  local key=$1
+  local metadata
+  metadata=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ".$key")
+  if [ -n "${metadata}" ] && [ "${metadata}" != "null" ]; then
+    echo "{ \"$key\": \"$metadata\" }"
+  else
+    echo "Error: Key not found in metadata."
+    exit 1
+  fi
 }
 
-# Run the main function
-main
+if [ -n "$1" ]; then
+  data_key=$1
+  get_specific_metadata_key "${data_key}"
+else
+  get_instance_metadata
+fi
