14992cf120369751b68f6284f519e70d974390ef
Mohit229123 <83329568+Mohit229123@users.noreply.github.com>
2023-08-01 09:14:02 +0530

    Updated db.tf

--- Terraform/db.tf
+++ Terraform/db.tf
@@ -46,7 +46,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.db.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
-  multi_az = true
+  multi_az = false
 
   availability_zone = "us-east-1a"
 
